a9a57252d11e8d6e4213a34e1dffdc76740bbf4a
DerekStride <djgstride@gmail.com>
2017-09-25 14:20:53 -0400

    Add graphql and graphiql

---
+++ app/graph/types/query_type.rb
@@ -0,0 +1,6 @@
+QueryType = GraphQL::ObjectType.define do
+  name 'Query'
+  description 'The query root for this schema'
+
+  field :simple, !types.String, resolve: -> (_, args, _) { SecureRandom.uuid }
+end
