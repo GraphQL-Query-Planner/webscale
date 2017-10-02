0820234a58cb0c8f13cca50be1a751afd38e65c4
DerekStride <djgstride@gmail.com>
2017-10-02 14:07:24 -0400

    Expose the models

--- app/graph/app_schema.rb
+++ app/graph/app_schema.rb
@@ -1,3 +1,6 @@
 AppSchema = GraphQL::Schema.define do
   query QueryType
+
+  id_from_object ->(object, _, _) { object.to_global_id.to_s }
+  resolve_type -> (type, obj, _ctx) { type.possible_types.detect { |t| t.name == obj.class.name } }
 end
--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -3,4 +3,27 @@ QueryType = GraphQL::ObjectType.define do
   description 'The query root for this schema'
 
   field :simple, !types.String, resolve: -> (_, args, _) { SecureRandom.uuid }
+
+  field :post, PostType, resolve: ActiveRecordResolver do
+    argument :id, !types.ID
+  end
+
+  field :photo, PhotoType, resolve: ActiveRecordResolver do
+    argument :id, !types.ID
+  end
+
+  field :comment, CommentType, resolve: ActiveRecordResolver do
+    argument :id, !types.ID
+  end
+
+  field :user, UserType, resolve: ActiveRecordResolver do
+    argument :id, !types.ID
+  end
+
+  field :like, LikeType do
+    argument :user_id, !types.ID
+    argument :content_id, !types.ID
+
+    resolve -> (_, args, _) { Like.find_by(user_id: args[:user_id], content_id: args[:content_id]) }
+  end
 end
