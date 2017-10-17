abf4d106a70e41040ead9ebd4caf521503338928
DerekStride <djgstride@gmail.com>
2017-10-17 12:24:25 -0400

    Add post tests

---
+++ spec/factories/post.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    body 'Hello, World!'
+    association :author, factory: :user
+    association :receiver, factory: :user
+  end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Post, type: :model do
+  context 'initialized with associations' do
+    let(:user) { build(:user) }
+    let(:post) { build(:post, author: user, receiver: user) }
+
+    it 'has an author' do
+      expect(post.author).to eq user
+    end
+
+    it 'has a receiver' do
+      expect(post.receiver).to eq user
+    end
+  end
+end
