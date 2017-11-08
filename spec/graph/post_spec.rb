633dc4b25867e49e1947806102471886c7e1fa27
Justin Krol <justin.krol@shopify.com>
2017-11-08 14:37:27 -0500

    Add test ensuring N+1 for authors of posts for a user

---
+++ spec/graph/post_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'explainer/parser'
+
+describe Post, type: :model do
+  let(:user) { create(:user) }
+  let(:posts) { FactoryGirl.create_list(:post, 5, author: user) }
+
+  it "should make N+1 queries for the authors of posts" do
+    user
+    posts
+    query_string = %|
+      {
+        user(id: "#{user.to_global_id}") {
+          id
+          posts {
+            author {
+              id
+            }
+          }
+        }
+      }
+    |
+
+    expect(user.posts.count).to eq 5
+    queries = ActiveRecord::Base.collecting_queries_for_explain do
+      AppSchema.execute(query_string, context: {}, variables: {})
+    end
+
+    expect(queries.count).to eq 7 # 1 for original user, 1 for posts, 5 for each post author.
+  end
+end
