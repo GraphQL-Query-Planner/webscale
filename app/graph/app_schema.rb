e53c46c9cd9646645802218310c9d1991ec57404
DerekStride <djgstride@gmail.com>
2017-11-14 21:46:05 -0500

    Fix exeception when id is invalid

--- app/graph/app_schema.rb
+++ app/graph/app_schema.rb
@@ -2,7 +2,7 @@ AppSchema = GraphQL::Schema.define do
   query QueryType
 
   id_from_object ->(object, _, _) { object.to_global_id.to_s }
-  object_from_id -> (id, _ctx) { GlobalID::Locator.locate(id) }
+  object_from_id -> (id, _ctx) { ActiveRecordResolver.call(nil, { id: id }, nil) }
   resolve_type -> (type, obj, _ctx) do
     if type.respond_to?(:possible_types)
       type.possible_types.detect { |t| t.name == obj.class.name }
