e6a8d68e9c3132aad57b99cc6025d327ee2bc419
Justin Krol <justin.krol@shopify.com>
2018-03-14 18:53:08 -0400

    Update query type to support some example queries

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -20,14 +20,15 @@ QueryType = GraphQL::ObjectType.define do
   end
 
   connection :posts, PostType.connection_type do
+    argument :max_id, types.ID
+    argument :min_id, types.ID
     argument :receiver_id, types.ID
 
     resolve -> (_, args, _) {
-      if args[:receiver_id]
-        Post.where(receiver_id: args[:receiver_id])
-      else
-        Post.all
-      end
+      id_cond = args[:min_id] && [:max_id] ? { id: args[:min_id]..args[:max_id] } : nil
+      receiver_cond = args[:receiver_id] ? { receiver_id: args[:receiver_id] } : nil
+
+      Post.where(receiver_cond).where(id_cond)
     }
   end
 
@@ -38,8 +39,27 @@ QueryType = GraphQL::ObjectType.define do
     resolve -> (_, args, _) { Comment.where(content_id: args[:content_id], content_type: args[:content_type]) }
   end
 
+  connection :likes, LikeType.connection_type do
+    argument :content_id, !types.ID
+    argument :content_type, !types.ID
+    argument :user_id, types.ID
+
+    resolve -> (_, args, _) {
+      user_cond = args[:user_id] ? { user_id: args[:user_id] } : nil
+      Like.ignore_index("index_likes_on_user_id_and_content_id_and_content_type").where(user_cond).where(content_id: args[:content_id], content_type: args[:content_type])
+    }
+  end
+
   connection :users, UserType.connection_type do
-    resolve -> (_, args, _) { User.all }
+    argument :first_name, types.String
+    argument :last_name, types.String
+
+    resolve -> (_, args, _) {
+      first_name_cond = args[:first_name] ? {first_name: args[:first_name]} : nil
+      last_name_cond = args[:last_name] ? {last_name: args[:last_name]} : nil
+
+      User.where(first_name_cond).where(last_name_cond)
+    }
   end
 
   field :like, LikeType do
