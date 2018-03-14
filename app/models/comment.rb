f254f59efdae42e6ca5f6a92eb6cdea327a44efb
DerekStride <djgstride@gmail.com>
2018-03-14 12:22:01 -0400

    Add es callbacks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
   belongs_to :author, class_name: 'User'
   belongs_to :content, polymorphic: true
